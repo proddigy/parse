c56a8ebb7bd4a00075441e959f5b9f9bac8675c3
proddigy <34798950+proddigy@users.noreply.github.com>
2017-12-27 16:02:41 +0300

    Add files via upload

---
+++ parse.py
@@ -0,0 +1,45 @@
+import csv
+from bs4 import BeautifulSoup
+import requests
+
+url = 'https://www3.lenovo.com/by/ru/laptops/c/Laptops'
+
+
+def get_html(url):
+    try:
+        response = requests.get(url, timeout = 5)
+        return response.content
+    except requests.exceptions.ConnectTimeout:
+        print('Oops. Connection timeout occured!')
+    except requests.exceptions.ConnectionError:
+        print('Oops. Connection error occured')
+    except requests.exceptions.MissingSchema:
+        print('Invalid URL')
+
+def save(names, path):
+    with open(path, 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(('Название', 'Описание'))
+
+        for name in names:
+            writer.writerow((name['title'], ''.join(name['description'])))
+
+
+def parse(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    names = []
+    number=0
+    for table in soup.find_all('div', class_ = 'model-title'):
+
+        names.append({
+            'title' : 'Lenovo '+table.text.strip(),
+            'description' : [i.text.strip() for i in soup.find_all('div', class_ = 'model-descript')][number]
+        })
+        number+=1
+    return names
+
+def main():
+    save(parse(get_html(url)), 'notebooks.csv')
+
+if __name__ == '__main__':
+    main()
